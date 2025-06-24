d6292aef674ed4567075d7fdb0ebefbe364560c8
Baptiste Leroux <lerouxbaptistepro2@gmail.com>
2025-06-24 21:03:00 +0200

    Validating train + added yaml file

--- train.py
+++ train.py
@@ -2,11 +2,35 @@ import os
 import torch
 from torchmetrics import Accuracy
 from tqdm import tqdm
+import yaml
 import matplotlib.pyplot as plt
+from torchsummary import summary
 
 import custom_dataset as custom_dataset
 from model import get_model
 import configuration as cfg
+
+
+def run_config_yaml(path):
+    """
+    read the config YAML file and return the dictionary
+    """
+    with open(path, "r") as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+
+def setup_training(model, stage):
+
+    if stage == "stage1":
+        for param in model.parameters():
+            param.requires_grad = False
+
+        for param in model.layer4.parameters():
+            param.requires_grad = True
+        for param in model.fc.parameters():
+            param.requires_grad = True
 
 
 def train(train_loader, val_loader, model, num_epochs, criterion, optimizer, device, scheduler=None):
@@ -61,6 +85,7 @@ def train(train_loader, val_loader, model, num_epochs, criterion, optimizer, dev
         print(
             f'Epoch {epoch+1}: Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f} | Val Accuracy: {val_accuracy:.4f}')
 
+
         if val_accuracy > best_accuracy:
             best_accuracy = val_accuracy
             torch.save(model.state_dict(), cfg.MODEL_WEIGHTS)
@@ -88,6 +113,8 @@ def train(train_loader, val_loader, model, num_epochs, criterion, optimizer, dev
             plt.close()
 
 if __name__ == "__main__":
+    config = run_config_yaml("config.yaml")
+
     dataset_train = custom_dataset.CustomDataset(
         csv_path=os.path.join(cfg.BASE_PATH, "train.csv"),
         transforms=custom_dataset.get_transform(train=True)
@@ -100,14 +127,18 @@ if __name__ == "__main__":
 
     train_data_loader = torch.utils.data.DataLoader(
         dataset_train,
-        batch_size=32,
+        batch_size=64,
         shuffle=True,
+        num_workers=2,
+        pin_memory=True
     )
 
     val_data_loader = torch.utils.data.DataLoader(
         dataset_valid,
-        batch_size=32,
+        batch_size=64,
         shuffle=True,
+        num_workers=2,
+        pin_memory=True
     )
 
     class_name = custom_dataset.get_classes_name(os.path.join(cfg.BASE_PATH, "classname.txt"))
@@ -115,17 +146,28 @@ if __name__ == "__main__":
 
     model = get_model(len(class_name)).to(cfg.DEVICE)
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=8, gamma=0.1)
-    num_epochs = 20
 
-    train(
-        train_data_loader,
-        val_data_loader,
-        model,
-        num_epochs,
-        criterion,
-        optimizer,
-        cfg.DEVICE,
-        scheduler=scheduler
-    )
+    for stage in list(config.keys()):
+        setup_training(model, stage)
+        current_stage = config[stage]
+
+        trainable_params = [p for p in model.parameters() if p.requires_grad]
+
+        optimizer = torch.optim.Adam(trainable_params, lr=current_stage["lr"], weight_decay=1e-5)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=current_stage["step_size"], gamma=current_stage["gamma"])
+        num_epochs = current_stage["num_epochs"]
+
+        print(f"Starting stage: {stage.upper()}")
+        print(f"Optimizer: {len(trainable_params)} paramètres, LR={current_stage['lr']}")
+        print(f"Training: {current_stage['num_epochs']} epochs")
+        print("-" * 40)
+        train(
+            train_data_loader,
+            val_data_loader,
+            model,
+            num_epochs,
+            criterion,
+            optimizer,
+            cfg.DEVICE,
+            scheduler=scheduler
+        )
